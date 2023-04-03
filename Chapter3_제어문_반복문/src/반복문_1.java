413613d4744e0bcaf49cb05302d656dd764b18d9
SIST <SIST@DESKTOP-F0502PF>
2023-04-03 13:54:22 +0900

    1111

---
+++ Chapter3_제어문_반복문/src/반복문_1.java
@@ -0,0 +1,19 @@
+/*
+ * 자바에서 지원하는 반복문
+ * ------------------
+ * for : 반복 횟수가 지정된 경우에 주로 사용	=> 2차(중첩) => 코테
+ * 	= 일반 for
+ * 	= for ~ each : 출력용(웹에서 가장 많이 등장: 향상된 for) 	***배열,컬렉션
+ * 
+ * while: 반복 횟수가 없는 경우 (선 조건)
+ * do~while: 반복횟수를 모르는 경우 (후조건) => 무조건 1번 이상은 수행이 가능하다
+ * 
+ */
+public class 반복문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
