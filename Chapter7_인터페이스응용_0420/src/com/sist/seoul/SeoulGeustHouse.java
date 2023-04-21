be7e420137a2ffe740b70c4ae652277388bd54b0
SIST <SIST@DESKTOP-F0502PF>
2023-04-21 09:03:11 +0900

    0420

--- Chapter7_인터페이스응용_0420/src/com/sist/seoul/SeoulGeustHouse.java
+++ Chapter7_인터페이스응용_0420/src/com/sist/seoul/SeoulGeustHouse.java
@@ -2,5 +2,22 @@ package com.sist.seoul;
 
 
 public class SeoulGeustHouse implements Seoul {
+	
+	@Override
+	public SeoulVO[] seoulList(int page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public SeoulVO[] seoulFind(String fd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SeoulVO seoulDetailData(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Chapter7_인터페이스응용_0420/src/com/sist/seoul/SeoulLocation.java
+++ Chapter7_인터페이스응용_0420/src/com/sist/seoul/SeoulLocation.java
@@ -2,6 +2,19 @@ package com.sist.seoul;
 
 public class SeoulLocation   {
 
-	
+	public SeoulVO[] seoulList(int page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SeoulVO[] seoulFind(String fd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SeoulVO seoulDetailData(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
